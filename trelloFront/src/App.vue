0b6496b758cae1fceda47a75b97cdcf332d4cef3
tristan <tristan3755@gmail.com>
2022-03-22 00:25:58 +0100

    feature/landingPage

---
+++ trelloFront/src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import landingPage from './components/landingPage.vue';
+</script>
+
+<template>
+<landingPage/>
+</template>
+
+<style lang="scss">
+body{
+    padding: 0;
+    margin: 0;
+}
+*{
+    box-sizing: border-box;
+}
+#app{
+    width: 100%;
+    height: auto;
+}
+</style>
